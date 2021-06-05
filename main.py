bec4a19e5b97ab7f54f208892d13bee75094b7b2
Serega <shreker240@gmail.com>
2021-06-05 18:00:35 +0300

    Homework #1

---
+++ main.py
@@ -0,0 +1,14 @@
+# import this - это импорт который выводит в консоли пасхалку
+# import antigravity - этот импорт переводит нас на сайт с комиксом
+# import __hello__ - импорт выводит надпись в консоли "Hello World"
+# import __future__ import braces - мы хотим импортировать скобки, а программа пишет: ни шанса
+from art import *
+
+tprint("SEREGA", font="*")
+x = 24
+y = 16
+print("Числа ",x, y, sep=" - ")
+print("Cумма - ",x + y)
+print("Деление - ",x / y)
+print("Остаток - ",x % y)
+print("Степень - ",x ** y)
