738fc43c480141e6f2d269b0ce4942bbb2a517ed
Serega <shreker240@gmail.com>
2021-07-07 12:41:15 +0300

    Homework #idk

--- homework3.py
+++ homework3.py
@@ -1,7 +1,7 @@
 from string import ascii_letters
+
 numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 checkList = list(ascii_letters) + numbers
-
 
 exceptionWord = ["содержит запрещенные символы"]
 exceptionOddNumbers = ["содержит четное кол-во цифр"]
@@ -38,16 +38,13 @@ def checkWords(word):
 
 
 def validate_password(password):
-    if checkLetters(password) == exceptionWord:
-        return exceptionWord
-    elif checkWords(password) != exceptionWord:
-        if checkNumbers(password) == exceptionOddNumbers:
-            return exceptionOddNumbers
-        elif checkNumbers(password) != exceptionOddNumbers:
-            if checkWords(password) == exceptionEvenLetters:
-                return exceptionEvenLetters
-            elif checkWords(password) != exceptionEvenLetters:
-                return True
+    exceptions = []
+
+    for element in [checkLetters(password), checkNumbers(password), checkWords(password)]:
+        if element != True:
+            exceptions += element
+
+    return exceptions
 
 
-print(validate_password("test228"))
+print(validate_password("sdists2222s@"))
